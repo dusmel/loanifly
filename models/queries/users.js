7e3b22ea1004be63c5ae36c4155075903fcf4656
Gisele Iradukunda <vera@Giseles-MacBook-Pro.local>
2019-03-27 14:44:08 +0200

    [feature #164831901] view users

--- models/queries/users.js
+++ models/queries/users.js
@@ -2,6 +2,7 @@ const userQueries = {
   create:
     'INSERT INTO users (name, email, password, role) VALUES ($1, $2, $3, $4) RETURNING *',
   login: 'SELECT * FROM users where email = $1',
+  getUsers: 'SELECT * FROM users',
 };
 
 export default userQueries;
