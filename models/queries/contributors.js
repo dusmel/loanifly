e549fe1fa14dfa25e353dd52b6419fae4ed3e242
mutombo jean-vincent <jean-vincent.mutombo@andela.com>
2019-03-27 20:59:23 +0200

    [Feature #164831890] Solving the conflit on contributorQuery

--- models/queries/contributors.js
+++ models/queries/contributors.js
@@ -1,10 +1,6 @@
 const contributorsQueries = {
   contribute:
-<<<<<<< HEAD
-    'INSERT INTO contributions (amount, status) VALUES ($1, $2) RETURNING *',
-=======
-    "INSERT INTO contributions (amount, owner) VALUES ($1, $2) RETURNING *"
->>>>>>> 15f57c1b0a3ed1bbb88dca6149337ff63a0a83f5
+    'INSERT INTO contributions (amount, owner) VALUES ($1, $2) RETURNING *',
 };
 
 export default contributorsQueries;
