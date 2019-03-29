5417a569981fcc87fb38c3c720b9f51237ac87da
Grace Lungu <gracelungu@Graces-MacBook-Pro.local>
2019-03-29 10:39:01 +0200

    [feature #164832218] Enable the contributor to see the total loans

--- models/queries/contributors.js
+++ models/queries/contributors.js
@@ -2,7 +2,7 @@ const contributorsQueries = {
   contribute:
     "INSERT INTO contributions (amount, owner) VALUES ($1, $2) RETURNING *",
   getContrubutions: "SELECT * FROM contributions WHERE owner=$1",
-  getLoans: "SELECT * FROM loans ",
+  getLoans: "SELECT * FROM loans WHERE status=0",
   getContrubutionById: "SELECT * FROM contributions WHERE id=$1",
   payContribution:
     "UPDATE contributions SET status = 1, paiddate = $1 WHERE id = $2 AND status = 0 RETURNING *"
