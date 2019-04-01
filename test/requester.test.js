688fda3643936327a8a63918002f5b851d51361f
mutombo jean-vincent <jean-vincent.mutombo@andela.com>
2019-04-01 20:45:02 +0200

    [Chore #164957660] Write test for requester last time

--- test/requester.test.js
+++ test/requester.test.js
@@ -15,6 +15,20 @@ const testRequester = (tokens, credentials) => {
         });
     });
   });
+
+  describe('Should place loan requester', () => {
+    test('loan request', async () => {
+      await request(app)
+        .post('/api/v1/loans')
+        .set('Authorization', tokens.requester)
+        .send({
+          amount: 2000,
+        })
+        .then((res) => {
+          expect(res.body.status).toBe(200);
+        });
+    });
+  });
 };
 
 export default testRequester;
